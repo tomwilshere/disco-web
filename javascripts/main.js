798d61562c234fc4319926737ac2b11de0512655
Tom Wilshere <tom.wilshere@gmail.com>
2014-10-01 00:49:10 +0100

    DISCO CRAZY.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+
+colors = ["Aqua", "Coral", "Lime", "Red", "Cyan", "DeepPink", "GreenYellow", "Purple", "Yellow"];
+
+speed = 300;
+
+function randomColor() {
+	return '#'+(0x1000000+(Math.random()/2 + 0.5)*0xffffff).toString(16).substr(1,6);
+	// return colors[Math.floor(Math.random() * colors.length)];
+}
+
+(function changeColor() {
+	$("body").stop().animate({backgroundColor: randomColor()}, speed);
+	setTimeout(arguments.callee, speed)
+})();
+
